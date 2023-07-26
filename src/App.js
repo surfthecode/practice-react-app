1c53cafa65888cf9a24a41a3f9d6c28d20278d26
surfthecode <surfthecode@gmail.com>
2023-07-26 11:10:31 +0100

    add App.css; add search.svg icon; update App.js with api url and async func to call api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+
+const apiKey = "65b2296c";
+const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
     <>
       <h1>App</h1>
